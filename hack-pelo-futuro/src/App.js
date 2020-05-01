b0e484f17cae99361b97fae4105e62a44d4b2f99
Smpontelli <stefanopontelli@hotmail.com>
2020-05-01 04:02:37 -0300

    pequenas mudanças no todo

--- hack-pelo-futuro/src/App.js
+++ hack-pelo-futuro/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-import Routes from './routes/index'
-import { BrowserRouter } from 'react-router-dom'
-=======
->>>>>>> 133e217e9543402ae0c9efb31a7bc3c9287203ee
+import Routes from './routes/index';
+import { BrowserRouter } from 'react-router-dom';
 
 import './global.css';
 
-import Routes from './routes/index';
-
 function App() {
-<<<<<<< HEAD
-  return (
-    <BrowserRouter>
-      <Routes/>
-   </BrowserRouter>
-  );
-=======
-	return <Routes />;
->>>>>>> 133e217e9543402ae0c9efb31a7bc3c9287203ee
+	return (
+		<BrowserRouter>
+			<Routes />
+		</BrowserRouter>
+	);
 }
 
 export default App;
