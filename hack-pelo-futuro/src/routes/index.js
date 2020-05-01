247dbc14e05d21690e58b3562a26434a5a0e1b33
Smpontelli <stefanopontelli@hotmail.com>
2020-05-01 04:16:03 -0300

    Adição das paginas do doador, usuario e fornecedor

--- hack-pelo-futuro/src/routes/index.js
+++ hack-pelo-futuro/src/routes/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Deposito from '../pages/Deposito/index';
 import SignIn from '../pages/SignIn/index';
 import SignUp from '../pages/SignUp/index';
+import DonatorDashboard from '../pages/DonatorDashboard/index';
+import SupplierDashboard from '../pages/SupplierDashboard/index';
+import UserDashboard from '../pages/UserDashboard/index';
 
 // import DashBoard from '../pages/dashboard/index'
 // import Repository from '../pages/repository/index'
@@ -14,6 +17,9 @@ const Routes = () => (
 			<Route path="/deposito" component={Deposito} />
 			<Route path="/signin" component={SignIn} />
 			<Route path="/signup" component={SignUp} />
+			<Route path="/donator" component={DonatorDashboard} />
+			<Route path="/supplier" component={SupplierDashboard} />
+			<Route path="/user" component={UserDashboard} />
 		</Switch>
 	</BrowserRouter>
 );
